4d224be956e5034e740071929e37e5ababb0a8cd
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-04-02 10:17:26 +0530

    create app module via bash commands

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TechhackModule } from './techhack/techhack.module';
 
 @Module({
-  imports: [],
+  imports: [TechhackModule],
   controllers: [AppController],
   providers: [AppService],
 })
