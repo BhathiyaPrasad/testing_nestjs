5ba53f81687b0bdbf0b1c9bcdbfe666b37e43901
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-04-05 16:56:58 +0530

    testing nest js CLI for  create s REST API

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TechhackModule } from './techhack/techhack.module';
+import { CrudModule } from './crud/crud.module';
 
 @Module({
-  imports: [TechhackModule],
+  imports: [TechhackModule, CrudModule],
   controllers: [AppController],
   providers: [AppService],
 })
