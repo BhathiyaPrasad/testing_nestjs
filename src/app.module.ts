7bda1428d45c8bf02b314fc352d56b83a007346c
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-04-05 17:13:32 +0530

    generate resolver class

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TechhackModule } from './techhack/techhack.module';
 import { CrudModule } from './crud/crud.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TechhackModule, CrudModule],
+  imports: [TechhackModule, CrudModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
